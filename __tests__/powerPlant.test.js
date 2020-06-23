eea2146ccda5420a90f1d5d02940384e0d54fe91
Chisato Murakami <kiwipom1031@gmail.com>
2020-06-23 13:40:18 -0700

    add multiple plants

--- __tests__/powerPlant.test.js
+++ __tests__/powerPlant.test.js
@@ -1,5 +1,5 @@
 
-import { storeState, plant, changeState } from './../src/powerPlant.js';
+import { storeState, plant1, plant2, changeState, plant1StateChanger, plant2StateChanger } from './../src/powerPlant.js';
 
 describe('plant grow functions', () => {
   let plant;
@@ -8,14 +8,12 @@ describe('plant grow functions', () => {
   });
 
   test('should create plant object with incremented water property', () => {
-    // let plant = {"kind": "tulip", "soil": 0, "water": 0, "light": 0}
     const hydrate = (plant) => {
       return {
         ...plant,
         water: (plant.water || 0) + 1
       }
     };
-    // hydrate();
     expect(hydrate(plant)).toEqual({"kind": "tulip", "soil": 0, "water": 1, "light": 0});
   });
 
@@ -26,7 +24,6 @@ describe('plant grow functions', () => {
         soil: (plant.soil || 0) + 1
       }
     };
-
     expect(feed(plant)).toEqual({"kind": "tulip", "soil": 1, "water": 0, "light": 0})
   })
 
@@ -44,15 +41,15 @@ describe('plant grow functions', () => {
 
     expect(feed(2)(plant)).toEqual({"kind": "tulip", "soil": 2, "water": 0, "light": 0})
   })
+
   test('should create abstracted storage of plant state', () => {
     
-    const stateChanger = storeState();
     const blueFood = changeState("soil")(5);
-    const fedPlant = stateChanger(blueFood);
+    const fedPlant = plant1StateChanger(blueFood);
     const blackSoil = changeState("soil")(7);
-    const superFedPlant = stateChanger(blackSoil);
+    const superFedPlant = plant2StateChanger(blackSoil);
     expect(fedPlant).toEqual({"kind": "tulip", "soil": 5, "water":0, "light": 0});
-    expect(superFedPlant).toEqual({"kind": "tulip", "soil": 12, "water":0, "light": 0})
+    expect(superFedPlant).toEqual({"kind": "rose", "soil": 7, "water":0, "light": 0})
   });
 
   
--- src/powerPlant.js
+++ src/powerPlant.js
@@ -11,18 +11,28 @@
 //     soil: (plant.soil || 0) + 1
 //   }
 // };
-export let plant = {"kind": "tulip", "soil": 0, "water": 0, "light": 0}
+export let plant1 = {"kind": "tulip", "soil": 0, "water": 0, "light": 0}
+export let plant2 = {"kind": "rose", "soil": 0, "water": 0, "light": 0}
 
-export const storeState = () => {
-  let currentState = plant;
+export const storeState1 = () => {
+  let currentState = plant1;
   return (stateChangeFunction) => {
     const newState = stateChangeFunction(currentState);
     currentState = {...newState};
     return newState;
   }
 }
-
-const stateChanger = storeState();
+export const storeState2 = () => {
+  let currentState = plant2;
+  return (stateChangeFunction) => {
+    const newState = stateChangeFunction(currentState);
+    currentState = {...newState};
+    return newState;
+  }
+}
+export const plant1StateChanger = storeState1()
+export const plant2StateChanger = storeState2() 
+// const stateChanger = storeState();
 
 export const changeState = (prop) => {
   return (value) => {
@@ -38,6 +48,6 @@ const giveLight = changeState("light");
 const blueFood = changeState("soil")(5);
 
 
-const fedPlant = stateChanger(blueFood);
+// const fedPlant = stateChanger(blueFood);
 
 
